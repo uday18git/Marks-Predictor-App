5be37956e010170cba789f8288ab8234fb485172
Uday <kcuday07@gmail.com>
2023-04-08 10:17:45 +0530

    data transformed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,11 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass # this is a decorator
+# just like providing what all the input things are required for this data ingestion component
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv') # train data will be stored in this path
     test_data_path: str=os.path.join('artifacts','test.csv') # test data will be stored in this path
@@ -38,4 +42,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
